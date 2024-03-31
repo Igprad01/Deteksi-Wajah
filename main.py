17441f17cea00e2ba2b8a554d7d42033ecf0c56f
igprad01 <igustiputra99@gmail.com>
2024-03-31 11:56:29 +0700

    initial commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ cam = cv2.VideoCapture(0)
 
 def settingCamera(kamera):
     bnw = cv2.cvtColor(kamera, cv2.COLOR_BGR2GRAY)
-    wajah = ref.detectMultiScale(bnw, scaleFactor=1.1)   
+    wajah = ref.detectMultiScale(bnw, 1.3,5)   
     return wajah
 
 
@@ -14,12 +14,6 @@ def box(kamera):
     for x,y,w,h in settingCamera(kamera):
         cv2.rectangle(kamera ,(x,y),(x+w,y+h),(255,255,0),2)
         
-        key = cv2.waitKey(1) & 0xFF
-        if key == 27 :
-            break
-        
-        cam.release()
-        cv2.destroyAllWindows()
 
             
 def main():
@@ -27,7 +21,14 @@ def main():
         _, kamera = cam.read()
         box(kamera)
         cv2.imshow("box camera", kamera)
-    
         
-if __name__ == "__main__": 
-    main()
+        key = cv2.waitKey(1) & 0xFF
+        if key == 27 :
+            break
+        
+        
+        cam.release()
+        cv2.destroyAllWindows()
+        
+
+result = main()
