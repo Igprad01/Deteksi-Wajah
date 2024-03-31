cf5537d6d710f6ab553b0f6eb82f3ad715b958ee
igprad01 <igustiputra99@gmail.com>
2024-03-31 12:12:51 +0700

    finishing

--- main.py
+++ main.py
@@ -6,29 +6,27 @@ cam = cv2.VideoCapture(0)
 
 def settingCamera(kamera):
     bnw = cv2.cvtColor(kamera, cv2.COLOR_BGR2GRAY)
-    wajah = ref.detectMultiScale(bnw, 1.3,5)   
+    wajah = ref.detectMultiScale(bnw,scaleFactor=1.1)
     return wajah
 
 
 def box(kamera):
     for x,y,w,h in settingCamera(kamera):
-        cv2.rectangle(kamera ,(x,y),(x+w,y+h),(255,255,0),2)
+        cv2.rectangle(kamera ,(x,y),(x+w,y+h),(255,255,0),6)
         
 
             
 def main():
     while True:
         _, kamera = cam.read()
+        
         box(kamera)
         cv2.imshow("box camera", kamera)
         
         key = cv2.waitKey(1) & 0xFF
         if key == 27 :
             break
-        
-        
-        cam.release()
-        cv2.destroyAllWindows()
-        
-
 result = main()
+
+cam.release()
+cv2.destroyAllWindows()
