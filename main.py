61f76d99c576110c9c052138888dc94979658b32
igprad01 <igustiputra99@gmail.com>
2024-04-01 07:46:21 +0700

    finishing

--- main.py
+++ main.py
@@ -1,32 +1,29 @@
 import cv2 
 
-ref = cv2.CascadeClassifier('ref.xml')
+ref = cv2.CascadeClassifier('ref/ref.xml')
 cam = cv2.VideoCapture(0)
 
 
 def settingCamera(kamera):
-    bnw = cv2.cvtColor(kamera, cv2.COLOR_BGR2GRAY)
-    wajah = ref.detectMultiScale(bnw,scaleFactor=1.1)
+    wajah = ref.detectMultiScale(kamera,scaleFactor=1.1, minSize=(40, 40))
     return wajah
 
 
 def box(kamera):
-    for x,y,w,h in settingCamera(kamera):
-        cv2.rectangle(kamera ,(x,y),(x+w,y+h),(255,255,0),6)
+    for x ,y,w,h in settingCamera(kamera):
+        cv2.rectangle(kamera ,(x,y),(x+w , y+h),(255,255,255),5)
         
-
-            
 def main():
     while True:
         _, kamera = cam.read()
-        
         box(kamera)
+        gray = cv2.cvtColor(kamera, cv2.COLOR_BGR2GRAY)
         cv2.imshow("box camera", kamera)
-        
         key = cv2.waitKey(1) & 0xFF
         if key == 27 :
             break
 result = main()
+
 
 cam.release()
 cv2.destroyAllWindows()
